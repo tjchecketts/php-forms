d1eda2b51a5e85fce797c0192e9970acfc6252cb
“TJ <tylerchecketts@gmail.com>
2018-04-17 14:17:46 -0600

    form values save after page reload

--- form.php
+++ form.php
@@ -1,27 +1,54 @@
 <?php 
+  $email = '';
+  $name = '';
+  $gender = '';
+  $color = '';
+  $cars = array();
+  $comments = '';
+  $terms = '';
+  
   if (isset($_POST['submit'])) {
     $ok = true;
 
-    if (
-      !isset($_POST['email']) 
-      || $_POST['email'] === '' 
-      || !isset($_POST['name']) 
-      || $_POST['name'] === '' 
-      || !isset($_POST['gender']) 
-      || $_POST['gender'] === '' 
-      || !isset($_POST['color']) 
-      || $_POST['color'] === '' 
-      || !isset($_POST['cars']) 
-      || !is_array($_POST['cars']) 
-      || count($_POST['cars']) === 0 
-      || !isset($_POST['comments']) 
-      || $_POST['comments'] === '' 
-      || !isset($_POST['terms']) 
-      || $_POST['terms'] === '' 
-    ) {
+    // requires each input and ensures valid data
+    if (!isset($_POST['email']) || $_POST['email'] === '' ) {
       $ok = false;
+    } else {
+      $email = $_POST['email'];
+    }
+    if (!isset($_POST['name']) || $_POST['name'] === '' ) {
+      $ok = false;
+    } else {
+      $name = $_POST['name'];
+    }
+    if (!isset($_POST['gender']) || $_POST['gender'] === '' ) {
+      $ok = false;
+    } else {
+      $gender = $_POST['gender'];
+    }
+    if (!isset($_POST['color']) || $_POST['color'] === '' ) {
+      $ok = false;
+    } else {
+      $color = $_POST['color'];
+    }
+    if (!isset($_POST['cars']) || !is_array($_POST['cars']) 
+      || count($_POST['cars']) === 0) {
+      $ok = false;
+    } else {
+      $cars = $_POST['cars'];
+    }
+    if (!isset($_POST['comments']) || $_POST['comments'] === '' ) {
+      $ok = false;
+    } else {
+      $comments = $_POST['comments'];
+    }
+    if (!isset($_POST['terms']) || $_POST['terms'] === '' ) {
+      $ok = false;
+    } else {
+      $terms = $_POST['terms'];
     }
 
+    // prints results
     if ($ok) {
       printf(
         'Your Inputs:
@@ -35,44 +62,97 @@
         <br>
         <br>
         ',
-          htmlspecialchars($_POST['email'], ENT_QUOTES),
-          htmlspecialchars($_POST['name'], ENT_QUOTES),
-          htmlspecialchars($_POST['gender'], ENT_QUOTES),
-          htmlspecialchars($_POST['color'], ENT_QUOTES),
-          htmlspecialchars(implode(' ', $_POST['cars']), ENT_QUOTES),
-          htmlspecialchars($_POST['comments'], ENT_QUOTES),
-          htmlspecialchars($_POST['terms'], ENT_QUOTES)
+          htmlspecialchars($email, ENT_QUOTES),
+          htmlspecialchars($name, ENT_QUOTES),
+          htmlspecialchars($gender, ENT_QUOTES),
+          htmlspecialchars($color, ENT_QUOTES),
+          htmlspecialchars(implode(' ', $cars), ENT_QUOTES),
+          htmlspecialchars($comments, ENT_QUOTES),
+          htmlspecialchars($terms, ENT_QUOTES)
       );
     }
   }
 ?>
+
+<!-- html form -->
 <form method='post' action=''>
   Email: <br>
-    <input type='text' name='email'><br>
+    <input type='text' name='email' value="<?php 
+      // retains values after page reload
+      echo htmlspecialchars($email); 
+    ?>"><br>
   User Name: <br>
-    <input type='text' name='name'><br>
+    <input type='text' name='name' value="<?php 
+      echo htmlspecialchars($name); 
+    ?>"><br>
   Gender: <br>
-    <input type="radio" name="gender" value="male"> Male<br>
-    <input type="radio" name="gender" value="female"> Female<br>
-    <input type="radio" name="gender" value="other"> Other<br>
+    <input type="radio" name="gender" value="male" <?php 
+      if ($gender === 'male') {
+        echo 'checked';
+      }
+    ?>> Male<br>
+    <input type="radio" name="gender" value="female" <?php 
+      if ($gender === 'female') {
+        echo 'checked';
+      }
+    ?>> Female<br>
+    <input type="radio" name="gender" value="other" <?php 
+      if ($gender === 'other') {
+        echo 'checked';
+      }
+    ?>> Other<br>
   Favorite color: <br>
     <select name='color'>
       <option value=''>Select Color</option>
-      <option value='#f00'>Red</option>
-      <option value='#0f0'>Green</option>
-      <option value='#00f'>Blue</option>
+      <option value='#f00' <?php 
+        if ($color === '#f00') {
+          echo 'selected';
+        }
+      ?>>Red</option>
+      <option value='#0f0' <?php 
+        if ($color === '#0f0') {
+          echo 'selected';
+        }
+      ?>>Green</option>
+      <option value='#00f' <?php 
+        if ($color === '#00f') {
+          echo 'selected';
+        }
+      ?>>Blue</option>
     </select><br>
   Select all favorite cars: <br>
   (Hold Ctrl or Command to add multiple)<br>
     <select name='cars[]' multiple>
-      <option value="volvo">Volvo</option>
-      <option value="tesla">Tesla</option>
-      <option value="hyundai">Hyundai</option>
-      <option value="audi">Audi</option>
+      <option value="volvo" <?php 
+        if (in_array('volvo', $cars)) {
+          echo 'selected';
+        }
+      ?>>Volvo</option>
+      <option value="tesla" <?php 
+        if (in_array('tesla', $cars)) {
+          echo 'selected';
+        }
+      ?>>Tesla</option>
+      <option value="hyundai" <?php 
+        if (in_array('hyundai', $cars)) {
+          echo 'selected';
+        }
+      ?>>Hyundai</option>
+      <option value="audi" <?php 
+        if (in_array('audi', $cars)) {
+          echo 'selected';
+        }
+      ?>>Audi</option>
     </select><br>
   Comments: <br>
-    <textarea name='comments'></textarea><br>
+    <textarea name='comments' value="<?php 
+      echo htmlspecialchars($comments); 
+    ?>"></textarea><br>
     I accept Terms and Conditions<br>
-    <input type='checkbox' name='terms' value='ok'><br>
+    <input type='checkbox' name='terms' value='ok' <?php 
+      if ($terms === 'ok') {
+        echo 'checked';
+      }
+    ?>><br>
     <input type='submit' name='submit' value='Submit'>
 </form>
